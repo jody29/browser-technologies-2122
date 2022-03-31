8c11610dc14e55d482a32c06171797863d0855f7
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-03-31 17:37:18 +0200

    add to cart works

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { v4: uuidv4 } = require('uuid')
+const fs = require('fs')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 const PORT = 8500
@@ -9,11 +12,56 @@ const PORT = 8500
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('pages/index', {
-        title: 'Customize T-Shirt'
+    if (req.query.edit) {
+        const data = JSON.parse(fs.readFileSync('./data.json'))
+        const shirt = data.data.find(
+            ({ id }) => id === req.query.edit
+        )
+
+        res.render('pages/index', {
+            shirt,
+            id: req.query.edit
+        })
+    } else {
+        res.render('pages/index', {
+            shirt: undefined,
+            id: uuidv4()
+        })
+    }
+})
+
+app.get('/shoppingBag', (req, res) => {
+    const data = JSON.parse(fs.readFileSync('./data.json'))
+    const amount = data.data.length
+
+    console.log(data)
+
+    res.render('pages/cart', {
+        data,
+        amount
     })
+})
+
+app.post('/addShoppingBag', (req, res) => {
+    const data = JSON.parse(fs.readFileSync('./data.json'))
+    const shirt = data.data.find(({ id }) => id == req.body.id)
+    
+    if (shirt) {
+        shirt.color = req.body.color
+        shirt.gender = req.body.gender
+        shirt.size = req.body.size
+        shirt.text = req.body.text
+    } else {
+        data.data.push(req.body)
+    }
+
+    const stringData = JSON.stringify(data, null, 2)
+    fs.writeFileSync('data.json', stringData)
+
+    res.redirect('/shoppingBag')
 })
 
 // Listen to port
